43ad9d5aefcd99eea770d34f1c37820f8fc0ca13
Josh Dicker <joshdicker@joshs-mac-mini.mynetworksettings.com>
2025-03-18 15:03:07 -0400

    completed phase-1-functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+const hq = 42
+const block = 264
+function distanceFromHqInBlocks(loc) {
+    return loc >= hq ? loc - hq : hq - loc
+}
+
+function distanceFromHqInFeet(loc) {
+    const cb = distanceFromHqInBlocks(loc)
+    return cb * block
+}
+
+function distanceTravelledInFeet(ptA, ptB) {
+    return ptA >= ptB ? (ptA - ptB) * block : (ptB - ptA) * block 
+}
+
+function calculatesFarePrice(ptA, ptB) {
+    const feet = distanceTravelledInFeet(ptA, ptB)
+    if(feet <= 400) {
+       return 0
+    } else {
+        if(feet > 400 && feet <= 2000 ) {
+            return (feet - 400) * .02
+        } else {
+            if(feet > 2000 && feet <= 2500 ) {
+                return 25
+            } else {
+                if(feet > 2500) {
+                    return 'cannot travel that far'
+                }
+            }
+        }
+    }
+}
+
+
+
